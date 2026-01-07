264001df0c9d8ab0dad678d37a58ee50d546059f
kabraaa <loredop05@gmail.com>
2026-01-07 14:36:56 +0100

    slideshow resuelto

--- js/script.js
+++ js/script.js
@@ -13,6 +13,40 @@ if (hamburger && navbar) {
 
 
 
+// SLIDESHOW
+document.querySelectorAll("[data-slider]").forEach(slider => {
+  const images = slider.querySelectorAll("img");
+  const nextBtn = slider.querySelector(".next");
+  const prevBtn = slider.querySelector(".prev");
+
+  let index = 0;
+
+  function showSlide(i) {
+    images.forEach(img => img.classList.remove("active"));
+    images[i].classList.add("active");
+  }
+
+  function nextSlide() {
+    index = (index + 1) % images.length;
+    showSlide(index);
+  }
+
+  function prevSlide() {
+    index = (index - 1 + images.length) % images.length;
+    showSlide(index);
+  }
+
+  nextBtn.addEventListener("click", nextSlide);
+  prevBtn.addEventListener("click", prevSlide);
+
+  // Auto slide cada 5 segundos
+  setInterval(nextSlide, 5000);
+});
+
+
+
+
+
 // CHARACTERS
 const contenedor = document.getElementById("lista-personajes");
 
